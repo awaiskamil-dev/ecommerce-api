949f9bbde5c4bc1b970b7b350a639b0fadd847b8
awaiskamil-dev <kamilawais@gmail.com>
2026-07-31 19:25:33 +0500

    Creating login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,7 +23,25 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send('Login route');
+  const {email, password} = req.body;
+
+  if(!email || !password){
+    throw new CustomError.BadRequestError('Please provide email and password');
+  }
+ 
+  const user = await User.findOne({email});
+  if(!user){
+    throw new CustomError.UnauthenticatedError('Invalid credentials');
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if(!isPasswordCorrect){
+    throw new CustomError.UnauthenticatedError('Invalid credentials');
+  }
+
+  const tokenUser = createTokenUser(user);
+  attachCookiesToResponse({res, user: tokenUser});
+
+  res.status(StatusCodes.OK).json({user: tokenUser});
 };
 
 const logout = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -36,7 +36,12 @@ UserSchema.pre('save', async function (){
     return;
   }
   const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(this.password, salt);
+  this.password = await bcrypt.hash(this.password, salt);
 });
+
+UserSchema.methods.comparePassword = async function (canditatePassword) {
+  const isMatch = await bcrypt.compare(canditatePassword, this.password);
+  return isMatch;
+};
 
 module.exports = mongoose.model('User', UserSchema);
